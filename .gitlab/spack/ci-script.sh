8c61a670246c1dc5a103c044a2196b17f9a677c1
Tara Drwenski <drwenski1@llnl.gov>
2026-04-16 10:05:31 -0700

    Add extra debug info to the CI

--- .gitlab/spack/ci-script.sh
+++ .gitlab/spack/ci-script.sh
@@ -20,6 +20,7 @@ spack ${MY_SPACK_DEBUG} config blame mirrors
 spack ${MY_SPACK_DEBUG} mirror rm buildcache-destination
 spack ${MY_SPACK_DEBUG} mirror add --oci-username-variable CI_REGISTRY_USER --oci-password-variable CI_REGISTRY_PASSWORD buildcache-destination oci://${CI_REGISTRY_IMAGE}/${SPACK_TARGET}
 spack ${MY_SPACK_DEBUG} config blame mirrors
+spack ${MY_SPACK_DEBUG} spec "/${SPACK_JOB_SPEC_DAG_HASH}"
 spack ${MY_SPACK_DEBUG} ci rebuild
 echo Building ${SPACK_JOB_SPEC_PKG_NAME} /${SPACK_JOB_SPEC_DAG_HASH}
 spack --color=always --backtrace install --include-build-deps --no-check-signature --use-buildcache=package:never,dependencies:only /${SPACK_JOB_SPEC_DAG_HASH}
