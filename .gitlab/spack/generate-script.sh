ce180b5d093b6c1938b2f230c2c32a25d972db29
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2026-04-28 15:23:13 +0200

    direct include of ci.yaml in spack config

--- .gitlab/spack/generate-script.sh
+++ .gitlab/spack/generate-script.sh
@@ -21,8 +21,10 @@ spack ${MY_SPACK_DEBUG} config blame mirrors
 spack ${MY_SPACK_DEBUG} mirror add --oci-username-variable CI_REGISTRY_USER --oci-password-variable CI_REGISTRY_PASSWORD buildcache-destination oci://${CI_REGISTRY_IMAGE}/${SPACK_TARGET}
 spack ${MY_SPACK_DEBUG} config blame mirrors
 spack ${MY_SPACK_DEBUG} concretize --fresh || spack ${MY_SPACK_DEBUG} concretize --fresh
-spack ${MY_SPACK_DEBUG} --color=always --config-scope "${CI_PROJECT_DIR}/.gitlab/spack" ci generate --check-index-only --artifacts-root "${CI_PROJECT_DIR}/jobs_scratch_dir" --output-file "${CI_PROJECT_DIR}/jobs_scratch_dir/pipeline.yml"
+#spack ${MY_SPACK_DEBUG} --color=always --config-scope "${CI_PROJECT_DIR}/.gitlab/spack" ci generate --check-index-only --artifacts-root "${CI_PROJECT_DIR}/jobs_scratch_dir" --output-file "${CI_PROJECT_DIR}/jobs_scratch_dir/pipeline.yml"
+spack ${MY_SPACK_DEBUG} --color=always ci generate --check-index-only --artifacts-root "${CI_PROJECT_DIR}/jobs_scratch_dir" --output-file "${CI_PROJECT_DIR}/jobs_scratch_dir/pipeline.yml"
 cp -rL .gitlab/spack/envs/shared-ci/${LCSCHEDCLUSTER} ${CI_PROJECT_DIR}/jobs_scratch_dir/concrete_environment/${LCSCHEDCLUSTER}
+cp -rL .gitlab/spack/ci.yaml ${CI_PROJECT_DIR}/jobs_scratch_dir/concrete_environment/ci.yaml
 mkdir -p ${CI_PROJECT_DIR}/jobs_scratch_dir/concrete_environment/spack_repo/llnl_radiuss
 cp -rL .gitlab/spack/envs/shared-ci/spack_repo/llnl_radiuss/repo.yaml ${CI_PROJECT_DIR}/jobs_scratch_dir/concrete_environment/spack_repo/llnl_radiuss/repo.yaml
 cp -rL .gitlab/spack/envs/shared-ci/spack_repo/llnl_radiuss/packages ${CI_PROJECT_DIR}/jobs_scratch_dir/concrete_environment/spack_repo/llnl_radiuss/packages
