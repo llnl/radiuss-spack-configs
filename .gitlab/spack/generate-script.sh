b5acb38cc6b09374c0c5e62e91949289506cfcd9
Adrien Bernede <51493078+adrienbernede@users.noreply.github.com>
2026-04-28 21:33:33 +0200

    Apply suggestions from code review
    Co-authored-by: Adrien Bernede <51493078+adrienbernede@users.noreply.github.com>

--- .gitlab/spack/generate-script.sh
+++ .gitlab/spack/generate-script.sh
@@ -21,7 +21,6 @@ spack ${MY_SPACK_DEBUG} config blame mirrors
 spack ${MY_SPACK_DEBUG} mirror add --oci-username-variable CI_REGISTRY_USER --oci-password-variable CI_REGISTRY_PASSWORD buildcache-destination oci://${CI_REGISTRY_IMAGE}/${SPACK_TARGET}
 spack ${MY_SPACK_DEBUG} config blame mirrors
 spack ${MY_SPACK_DEBUG} concretize --fresh || spack ${MY_SPACK_DEBUG} concretize --fresh
-#spack ${MY_SPACK_DEBUG} --color=always --config-scope "${CI_PROJECT_DIR}/.gitlab/spack" ci generate --check-index-only --artifacts-root "${CI_PROJECT_DIR}/jobs_scratch_dir" --output-file "${CI_PROJECT_DIR}/jobs_scratch_dir/pipeline.yml"
 spack ${MY_SPACK_DEBUG} --color=always ci generate --check-index-only --artifacts-root "${CI_PROJECT_DIR}/jobs_scratch_dir" --output-file "${CI_PROJECT_DIR}/jobs_scratch_dir/pipeline.yml"
 cp -rL .gitlab/spack/envs/shared-ci/${LCSCHEDCLUSTER} ${CI_PROJECT_DIR}/jobs_scratch_dir/concrete_environment/${LCSCHEDCLUSTER}
 cp -rL .gitlab/spack/ci.yaml ${CI_PROJECT_DIR}/jobs_scratch_dir/concrete_environment/ci.yaml
